c6727bbee6138f8dc4f588cfd7f17149779452cc
krishna-tech1 <muthukrishnan8733@gmail.com>
2026-02-17 03:09:17 +0530

    Cleanup: Remove temporary scripts and log files

--- push_to_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo "# school_web_portal" > README.md
-git init
-git add .
-git commit -m "Initial commit: School ERP Frontend"
-git branch -M main
-git remote add origin https://github.com/krishna-tech1/school_web_portal.git
-git push -u origin main
